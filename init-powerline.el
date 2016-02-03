9a274d381a2da7df5c170b3f590f569407f56a3f
mriehl <max@riehl.io>
2016-02-03 10:27:35 +0100

    bit lighter background for active powerline

--- init-powerline.el
+++ init-powerline.el
@@ -7,6 +7,7 @@
 (custom-set-faces
  '(mode-line-inactive ((t (:background "brightwhite" :foreground "color-244" :box (:line-width 1 :color "gray40")))))
  '(powerline-active1 ((t (:inherit mode-line :background "black" :foreground "white"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "grey"))))
  '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "brightwhite" :foreground "color-243"))))
 '(spaceline-evil-insert ((t (:background "tomato" :foreground "#3E3D31" :inherit (quote mode-line)))))
 '(spaceline-evil-normal ((t (:background "green" :foreground "#3E3D31" :inherit (quote mode-line)))))
