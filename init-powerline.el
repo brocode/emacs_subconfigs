0a698d88c73a3ea1300890e793495c4afe21285e
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-02-03 17:10:39 +0100

    Use hex values for colors
    Window mode and terminal mode have different color names. Hex values
    work in both.

--- init-powerline.el
+++ init-powerline.el
@@ -5,12 +5,15 @@
 (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
 
 (custom-set-faces
- '(mode-line-inactive ((t (:background "brightwhite" :foreground "color-244" :box (:line-width 1 :color "gray40")))))
- '(powerline-active1 ((t (:inherit mode-line :background "black" :foreground "white"))))
- '(powerline-active2 ((t (:inherit mode-line :background "grey"))))
- '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "brightwhite" :foreground "color-243"))))
-'(spaceline-evil-insert ((t (:background "tomato" :foreground "#3E3D31" :inherit (quote mode-line)))))
-'(spaceline-evil-normal ((t (:background "green" :foreground "#3E3D31" :inherit (quote mode-line)))))
- '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "white")))))
+ '(mode-line-inactive ((t (:background "#EEEEEE" :foreground "#AAAAAA" :box (:line-width 1 :color "#666666")))))
+ '(powerline-active1 ((t (:inherit mode-line :background "#444444" :foreground "#FFFFFF"))))
+ '(powerline-active2 ((t (:inherit mode-line :background "#BBBBBB"))))
+ '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "#FFFFFF" :foreground "#AAAAAA"))))
+ '(spaceline-evil-insert ((t (:background "#FF6347" :foreground "#FFFFFF" :inherit (quote mode-line)))))
+ '(spaceline-evil-normal ((t (:background "#00cd00" :foreground "#FFFFFF" :inherit (quote mode-line)))))
+ '(spaceline-evil-visual ((t (:background "#ffaf00" :foreground "#FFFFFF" :inherit (quote mode-line)))))
+ '(spaceline-evil-emacs ((t (:background "#af00d7" :foreground "#FFFFFF" :inherit (quote mode-line)))))
+ '(spaceline-evil-replace ((t (:background "#5f97ff" :foreground "#FFFFFF" :inherit (quote mode-line)))))
+ '(powerline-inactive2 ((t (:inherit mode-line-inactive :background "#e5e5e5")))))
 
 (provide 'init-powerline)
