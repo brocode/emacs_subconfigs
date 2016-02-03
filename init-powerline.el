8a4201604715a1ee993daa398db4d82146e0717d
mriehl <max@riehl.io>
2016-02-03 10:24:29 +0100

    fix windowed emacs powerline
    The colors cause the powerline code to crash (no idea why). But now it
    works *and* looks good.

--- init-powerline.el
+++ init-powerline.el
@@ -5,10 +5,8 @@
 (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
 
 (custom-set-faces
- '(ensime-implicit-highlight ((t (:foreground "color-177"))))
  '(mode-line-inactive ((t (:background "brightwhite" :foreground "color-244" :box (:line-width 1 :color "gray40")))))
- '(powerline-active1 ((t (:inherit mode-line :background "brightblack" :foreground "brightwhite"))))
- '(powerline-active2 ((t (:inherit mode-line :background "color-252"))))
+ '(powerline-active1 ((t (:inherit mode-line :background "black" :foreground "white"))))
  '(powerline-inactive1 ((t (:inherit mode-line-inactive :background "brightwhite" :foreground "color-243"))))
 '(spaceline-evil-insert ((t (:background "tomato" :foreground "#3E3D31" :inherit (quote mode-line)))))
 '(spaceline-evil-normal ((t (:background "green" :foreground "#3E3D31" :inherit (quote mode-line)))))
