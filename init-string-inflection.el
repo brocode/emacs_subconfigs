fdca1c776ae9c5ca2041813e93c651b937a357a5
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-02-11 09:11:08 +0100

    add lisp case

--- init-string-inflection.el
+++ init-string-inflection.el
@@ -4,5 +4,6 @@
 (define-key evil-normal-state-map (kbd "C-c c c") 'string-inflection-camelcase)
 (define-key evil-normal-state-map (kbd "C-c c u") 'string-inflection-upcase)
 (define-key evil-normal-state-map (kbd "C-c c s") 'string-inflection-underscore)
+(define-key evil-normal-state-map (kbd "C-c c l") 'string-inflection-lisp)
 
 (provide 'init-string-inflection)
