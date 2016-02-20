4569c147565d406430c82dca4334eef03ca1be50
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-02-20 20:11:17 +0100

    default to unix line endings

--- init-sensible.el
+++ init-sensible.el
@@ -62,10 +62,10 @@
       inhibit-startup-message t)
 
 ;; UTF-8 everything!
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-selection-coding-system 'utf-8-unix)
+(prefer-coding-system 'utf-8-unix)
 
 ;; Flash the frame to represent a bell.
 (setq visible-bell t)
