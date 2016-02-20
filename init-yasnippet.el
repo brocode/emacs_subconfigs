3acf0448f8a3e93924da48f5bf5791ba1580b969
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-02-20 20:08:12 +0100

    snippets dir in subconfigs

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,6 +1,8 @@
 (require-package 'yasnippet)
 (require 'yasnippet)
 
+(setq yas-snippet-dirs '("~/.emacs.d/subconfigs/snippets" yas-installed-snippets-dir))
+
 (yas-global-mode 1)
 
 (provide 'init-yasnippet)
