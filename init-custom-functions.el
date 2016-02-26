b02f49c248bf375b7fc390cf3b43f0a61fd9264a
mriehl <max@riehl.io>
2016-02-26 15:59:21 +0100

    add "what-face" function

--- init-custom-functions.el
+++ init-custom-functions.el
@@ -12,4 +12,10 @@ Version 2015-06-11"
             'face (list :background (match-string-no-properties 0)))))))
       (font-lock-fontify-buffer))
 
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+        (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
 (provide 'init-custom-functions)
