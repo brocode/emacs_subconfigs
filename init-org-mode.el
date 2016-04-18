253a2cddc9509c302adbb15c13f5c55ede37908b
mriehl <max@flatmap.ninja>
2016-04-18 08:42:54 +0200

    better org mode

--- init-org-mode.el
+++ init-org-mode.el
@@ -1,5 +1,27 @@
+(require-package 'org-bullets)
+
 ; syntax highlightning in org mode snippets
 (setq org-src-fontify-natively t)
+
+; hide *emphasis* /markers/
+(setq org-hide-emphasis-markers t)
+
+; better bullets
+(font-lock-add-keywords 'org-mode '(("^ +\\([-*]\\) " (0 (prog1 ()
+  (compose-region (match-beginning 1) (match-end
+    1) "•"))))))
+
+; better header bullets
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(custom-set-faces
+ '(org-level-1 ((t (:inherit outline-1 :height 1.75))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.25))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+ )
+
 
 ; org-latex-listings: nil means the default verbatim block export,
 ; 'minted means the minted package and any non-nil value other
