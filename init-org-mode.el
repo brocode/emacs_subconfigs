4d1f2f9237cd3137de944ef486f2d4fad6d8b260
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-04-18 16:02:53 +0200

    smaller headlines

--- init-org-mode.el
+++ init-org-mode.el
@@ -16,9 +16,9 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (custom-set-faces
- '(org-level-1 ((t (:inherit outline-1 :height 1.75))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.25))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.25))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
  )
 
