8bb2ac839db498d90f9c5674854c6eb92e191f6b
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-04-20 17:16:57 +0200

    customize font lock built in face

--- init-theme.el
+++ init-theme.el
@@ -4,5 +4,6 @@
 (custom-set-faces
  '(lazy-highlight ((t (:background "#00aaff" :foreground "#ffffff"))))
  '(font-lock-function-name-face ((t (:foreground "#8a2be2"))))
+ '(font-lock-builtin-face ((t (:foreground "#483d8b"))))
 )
 (provide 'init-theme)
