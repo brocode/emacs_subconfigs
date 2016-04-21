67740a156033ffe8e8b70bec03829e6933c09408
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-04-21 08:25:39 +0200

    use darker green for helm grep

--- init-theme.el
+++ init-theme.el
@@ -5,5 +5,6 @@
  '(lazy-highlight ((t (:background "#00aaff" :foreground "#ffffff"))))
  '(font-lock-function-name-face ((t (:foreground "#8a2be2"))))
  '(font-lock-builtin-face ((t (:foreground "#483d8b"))))
+ '(helm-grep-finish ((t (:foreground "#2e8b57"))))
 )
 (provide 'init-theme)
