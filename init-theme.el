f0c5d8a2ffa3a7d31155340b0ee2906d8ac27267
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-04-25 14:05:33 +0200

    bold fonts

--- init-theme.el
+++ init-theme.el
@@ -4,7 +4,8 @@
 (custom-set-faces
  '(lazy-highlight ((t (:background "#00aaff" :foreground "#ffffff"))))
  '(font-lock-function-name-face ((t (:foreground "#8a2be2"))))
- '(font-lock-builtin-face ((t (:foreground "#483d8b"))))
+ '(font-lock-builtin-face ((t (:foreground "#483d8b" :weight bold))))
+ '(font-lock-keyword-face ((t (:weight bold))))
  '(helm-grep-finish ((t (:foreground "#2e8b57"))))
 )
 (provide 'init-theme)
