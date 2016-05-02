8afdc6fc16e967798de04b97fd85364d13210bc6
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-05-02 21:15:03 +0200

    more use-package mapping

--- init-string-inflection.el
+++ init-string-inflection.el
@@ -1,12 +1,13 @@
 (use-package string-inflection
   :ensure t
-  :defer t
   :after evil
-  :config
-  (define-key evil-normal-state-map (kbd "C-c c c") 'string-inflection-camelcase)
-  (define-key evil-normal-state-map (kbd "C-c c u") 'string-inflection-upcase)
-  (define-key evil-normal-state-map (kbd "C-c c s") 'string-inflection-underscore)
-  (define-key evil-normal-state-map (kbd "C-c c l") 'string-inflection-lisp))
+  :defer t
+  :bind(
+        :map evil-normal-state-map
+             ("C-c c c" . string-inflection-camelcase)
+             ("C-c c u" . string-inflection-upcase)
+             ("C-c c s" . string-inflection-underscore)
+             ("C-c c l" . string-inflection-lisp)))
 
 
 (provide 'init-string-inflection)
