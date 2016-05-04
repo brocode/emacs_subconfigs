b18ad92dbe73192a635ee7a5afa3c0964d18e0b6
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-05-04 10:29:01 +0200

    start with tab

--- init-company.el
+++ init-company.el
@@ -8,8 +8,24 @@
   (define-key company-active-map (kbd "C-p") #'company-select-previous)
   (define-key company-active-map [tab] 'company-complete-common-or-cycle)
   (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
+  (define-key company-mode-map [remap indent-for-tab-command]
+    'company-indent-for-tab-command)
+
+  (setq tab-always-indent 'complete)
+
+  (defvar completion-at-point-functions-saved nil)
+
+  (defun company-indent-for-tab-command (&optional arg)
+    (interactive "P")
+    (let ((completion-at-point-functions-saved completion-at-point-functions)
+          (completion-at-point-functions '(company-complete-common-wrapper)))
+      (indent-for-tab-command arg)))
+
+  (defun company-complete-common-wrapper ()
+    (let ((completion-at-point-functions completion-at-point-functions-saved))
+      (company-complete-common)))
   :diminish company-mode
-)
+  )
 
 (use-package company-quickhelp          ; Show help in tooltip
   :ensure t
@@ -26,7 +42,7 @@
   :defer t
   :after company helm
   :init ;; Use Company for completion
-  ;(bind-key [remap completion-at-point] #'helm-company company-mode-map)
+                                        ;(bind-key [remap completion-at-point] #'helm-company company-mode-map)
   (bind-key "C-;" #'helm-company company-mode-map)
   (bind-key "C-;" #'helm-company company-active-map))
 
