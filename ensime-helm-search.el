ad0e4ced5c7d119a33bac088680cb3880dde9f67
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-05-24 17:08:23 +0200

    Make it work

--- ensime-helm-search.el
+++ ensime-helm-search.el
@@ -12,8 +12,8 @@
                   :candidates 'ensime-helm-public-symbol-search
                   :volatile t
                   :requires-pattern ensime-search-min-length
-                  :action '(("open file" . (lambda (candidate) (message "selected: %s" (helm-marked-candidates))))
-                            ("print info" . (lambda (candidate) (message "selected: %s" (helm-marked-candidates)))))
+                  :action '(("open" . ensime-helm-open-candidate)
+                            ("print info" . (lambda (candidate) (message "selected: %s" candidate))))
                   )
         :buffer "*ensime-helm-search*"))
 
@@ -27,3 +27,31 @@
      ,max-results
      )))
 
+(defun ensime-helm-open-candidate (candidate)
+  "Opens the candidate"
+  (let ((pos (ensime-search-sym-pos candidate)))
+      (let* ((file-name (ensime-pos-file pos))
+                  (line (ensime-pos-line pos))
+      (offset (ensime-pos-offset pos)))
+        (if (and file-name
+          (integerp (string-match
+          "\\.scala$\\|\\.java$"
+          file-name)))
+      (progn
+        (find-file file-name)
+                    (cond
+                    (offset (goto-char (ensime-internalize-offset offset)))
+                    (line (ensime-goto-line line))))
+
+          ;; Otherwise, open the inspector
+          (let ((decl-as (ensime-search-sym-decl-as item)))
+      (cond
+      ((or (equal decl-as 'method)
+            (equal decl-as 'field))
+        (ensime-inspect-by-path
+        (ensime-search-sym-owner-name item)
+        (ensime-search-sym-local-name item)
+        ))
+
+      (t (ensime-inspect-by-path
+          (ensime-search-sym-name item)))))))))
