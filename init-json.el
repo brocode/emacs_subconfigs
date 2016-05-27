a63025c1cf5e3b461a6f2dba7e79025c323b89b3
mriehl <max@flatmap.ninja>
2016-05-27 11:25:13 +0200

    disable flyspell in json files

--- init-json.el
+++ init-json.el
@@ -1,9 +1,10 @@
 (use-package json-mode
   :ensure t
   :defer t
-  :after flycheck-mode
+  :after flycheck-mode flyspell-mode
   :config
   (add-hook 'json-mode-hook 'flycheck-mode)
+  (add-hook 'json-mode-hook 'flyspell-mode-off)
   )
 
 (provide 'init-json)
