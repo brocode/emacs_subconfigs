30e4db2b83d02226b7254d38d7804e11bb0f57dd
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-05-30 17:12:05 +0200

    disable yas global mode

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -3,8 +3,6 @@
   :diminish yas-minor-mode
   :defer t
   :config
-  (setq yas-snippet-dirs '("~/.emacs.d/subconfigs/snippets" yas-installed-snippets-dir))
-
-  (yas-global-mode 1))
+  (setq yas-snippet-dirs '("~/.emacs.d/subconfigs/snippets" yas-installed-snippets-dir)))
 
 (provide 'init-yasnippet)
