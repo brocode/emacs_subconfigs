a02ce79d3044474d6fcfcdf0a8b756374f6004c5
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-09-27 09:55:29 +0200

    fix completion

--- init-elastic.el
+++ init-elastic.el
@@ -2,6 +2,7 @@
   :ensure t
   :after company
   :init
+  (evil-define-key 'insert es-mode-map (kbd "TAB") #'company-complete-common-or-cycle)
   (setq es-always-pretty-print t))
 
 (provide 'init-elastic)
