2249158dc164c58ba05c7ea3ecb1d45a30c6d6c5
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-09-28 15:52:04 +0200

    enable smartparens globally

--- init-smartparens.el
+++ init-smartparens.el
@@ -1,11 +1,14 @@
 (use-package smartparens
   :ensure t
+  :diminish smartparens-mode
   :init
   (require 'smartparens-config)
   (setq sp-highlight-pair-overlay nil)
   (setq sp-highlight-wrap-overlay nil)
   (setq sp-highlight-wrap-tag-overlay nil)
-  (add-hook 'emacs-lisp-mode-hook #'smartparens-mode))
+  ; in case global mode turns out to be annoying as hell use this instead
+  ;(add-hook 'emacs-lisp-mode-hook #'smartparens-mode)
+  (smartparens-global-mode t))
 
 (use-package evil-cleverparens
   :after smartparens
