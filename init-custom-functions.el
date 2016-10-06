633a3cedc0eda8b7cf427a56cc318ef6493a31ec
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-10-06 16:28:45 +0200

    add custom display ansi color function

--- init-custom-functions.el
+++ init-custom-functions.el
@@ -2,6 +2,12 @@
   (interactive "d")
   (let ((face (or (get-char-property (point) 'read-face-name)
                   (get-char-property (point) 'face))))
-        (if face (message "Face: %s" face) (message "No face at %d" pos))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
+
+(require 'ansi-color)
+(defun display-ansi-colors ()
+  (interactive)
+  (ansi-color-apply-on-region (point-min) (point-max)))
+
 
 (provide 'init-custom-functions)
