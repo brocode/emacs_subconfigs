219c70053fdc75c65cd5dce5ebbc3a24cd730784
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-10-18 13:55:43 +0200

    map barf and slurp

--- init-smartparens.el
+++ init-smartparens.el
@@ -6,6 +6,8 @@
   (setq sp-highlight-pair-overlay nil)
   (setq sp-highlight-wrap-overlay nil)
   (setq sp-highlight-wrap-tag-overlay nil)
+  (evil-define-key 'normal smartparens-mode-map (kbd "<") 'sp-forward-barf-sexp)
+  (evil-define-key 'normal smartparens-mode-map (kbd ">") 'sp-forward-slurp-sexp)
   (add-hook 'emacs-lisp-mode-hook #'smartparens-mode))
 
 (provide 'init-smartparens)
