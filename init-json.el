271464ba556f6e460ac85eecb25e3329b1f40e03
Patrick Haun <patrick.haun@immobilienscout24.de>
2016-10-28 15:23:18 +0200

    bind json pretty print buffer

--- init-json.el
+++ init-json.el
@@ -5,6 +5,7 @@
   :config
   (add-hook 'json-mode-hook 'flycheck-mode)
   (add-hook 'json-mode-hook 'flyspell-mode-off)
+  (define-key json-mode-map (kbd "M-f") 'json-pretty-print-buffer)
   )
 
 (provide 'init-json)
