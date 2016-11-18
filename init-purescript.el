7f8cab51b0fc61e63f8934c68de25240e84d882c
Patrick Haun <bomgar85@googlemail.com>
2016-11-18 08:20:51 +0100

    company is always enabled

--- init-purescript.el
+++ init-purescript.el
@@ -13,7 +13,6 @@
   (add-hook 'purescript-mode-hook
             (lambda ()
               (psc-ide-mode)
-              (company-mode)
               (flycheck-mode)
               (turn-on-purescript-indentation))))
 
