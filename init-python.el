a1d6056b7030af16ebf4457266556bbdd6cbe83d
mriehl <max@flatmap.ninja>
2017-01-05 13:30:21 +0100

    keybind jedi goto definition

--- init-python.el
+++ init-python.el
@@ -5,6 +5,7 @@
     (add-to-list 'company-backends 'company-jedi))
 
   (add-hook 'python-mode-hook 'my/python-mode-hook)
+  (define-key jedi-mode-map (kbd "M-d") 'jedi:goto-definition)
 )
 
 (provide 'init-python)
