22fae101241268f079b7936bbd951907395d54d6
Patrick Haun <bomgar85@googlemail.com>
2017-04-22 14:27:21 +0200

    enable yas snippet globally

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -2,18 +2,19 @@
   :ensure t
   :diminish yas-minor-mode
   :defer t
-  :config
-  (setq yas-snippet-dirs '("~/.emacs.d/subconfigs/snippets" yas-installed-snippets-dir))
-  (define-key yas-minor-mode-map [(tab)]        nil)
-  (define-key yas-minor-mode-map (kbd "TAB")    nil)
-  (define-key yas-minor-mode-map (kbd "<tab>")  nil)
-  ;(define-key yas-minor-mode-map (kbd "C-c y")  'yas-expand)
-  (define-key yas-keymap [(tab)] nil)
-  (define-key yas-keymap [(backtab)] nil)
-  (define-key yas-keymap (kbd "TAB") nil)
-  (define-key yas-keymap (kbd "<tab>") nil)
-  (define-key yas-keymap (kbd "C-l") 'yas-next-field)
-  (define-key yas-keymap (kbd "C-h") 'yas-prev-field)
-  )
+  :init (progn
+          (setq yas-snippet-dirs '("~/.emacs.d/subconfigs/snippets" yas-installed-snippets-dir))
+          (yas-global-mode))
+  :config (progn
+            (define-key yas-minor-mode-map [(tab)]        nil)
+            (define-key yas-minor-mode-map (kbd "TAB")    nil)
+            (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+            (define-key yas-minor-mode-map (kbd "C-c y")  'yas-expand)
+            (define-key yas-keymap [(tab)] nil)
+            (define-key yas-keymap [(backtab)] nil)
+            (define-key yas-keymap (kbd "TAB") nil)
+            (define-key yas-keymap (kbd "<tab>") nil)
+            (define-key yas-keymap (kbd "C-l") 'yas-next-field)
+            (define-key yas-keymap (kbd "C-h") 'yas-prev-field)))
 
 (provide 'init-yasnippet)
