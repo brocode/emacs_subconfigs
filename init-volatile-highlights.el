238fa69a47017f6a973661f7c00073831099cafe
Patrick Haun <bomgar85@googlemail.com>
2018-06-19 16:28:41 +0200

    customize color

--- init-volatile-highlights.el
+++ init-volatile-highlights.el
@@ -5,6 +5,7 @@
     (vhl/define-extension 'evil 'evil-paste-after 'evil-paste-before
                           'evil-paste-pop 'evil-move)
     (vhl/install-extension 'evil)
-)
+  (custom-set-faces
+  '(secondary-selection ((t (:background "#ffffb3"))))))
 
 (provide 'init-volatile-highlights)
