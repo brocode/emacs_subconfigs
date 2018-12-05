c84b5751d6b1081104570de42c67dfb9d7f5cabc
Patrick Haun <patrick@bomgar.email>
2018-12-05 16:28:58 +0100

    [feat] define evil jump

--- init-elixir.el
+++ init-elixir.el
@@ -4,6 +4,7 @@
 (use-package alchemist
   :ensure t
   :config
+    (evil-set-command-property 'alchemist-goto-definition-at-point :jump t)
     (define-key alchemist-mode-map (kbd "C-]") 'alchemist-goto-definition-at-point)
     (define-key alchemist-mode-map (kbd "C-d") 'alchemist-goto-definition-at-point))
 
