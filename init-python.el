0dfaa00b87fce17ef1db5e6c5d5c878e3027b7ee
Patrick Haun <patrick@bomgar.email>
2019-04-24 08:30:38 +0200

    [conf] add python lsp
    ping @mriehl

--- init-python.el
+++ init-python.el
@@ -1,11 +1,6 @@
-(use-package company-jedi
+(use-package python-mode
   :ensure t
-  :config
-  (defun my/python-mode-hook ()
-    (add-to-list 'company-backends 'company-jedi))
-
-  (add-hook 'python-mode-hook 'my/python-mode-hook)
-  (define-key jedi-mode-map (kbd "M-d") 'jedi:goto-definition)
-  (define-key jedi-mode-map (kbd "C-]") 'jedi:goto-definition))
+  :init
+  (add-hook 'python-mode-hook #'lsp))   ; sudo pip install python-language-server[all]
 
 (provide 'init-python)
