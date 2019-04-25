3371bb55d7079ae21cbe019727958a833bdbd7fd
Patrick Haun <patrick@bomgar.email>
2019-04-25 14:42:36 +0200

    [doc] lang server
    ping @mriehl

--- init-python.el
+++ init-python.el
@@ -1,6 +1,11 @@
 (use-package python-mode
   :ensure t
   :init
-  (add-hook 'python-mode-hook #'lsp))   ; sudo pip install python-language-server[all]
+  (add-hook 'python-mode-hook #'lsp))   ; pip install
+                                        ; python-language-server[all]. Don't
+                                        ; use all if you don't want
+                                        ; all optional packages. Mak
+                                        ; sure to start emacs with the
+                                        ; venv already activated
 
 (provide 'init-python)
