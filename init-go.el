ca8b71d5f29f236dd24ea8f70de41efd6f6ffc38
Patrick Haun <patrick@bomgar.email>
2019-04-26 13:54:03 +0200

    [feat] gopls
    ping @mriehl

--- init-go.el
+++ init-go.el
@@ -1,25 +1,11 @@
 (use-package go-mode
   :ensure t
   :defer t
-  :bind (:map go-mode-map
-        ("M-t" . godef-describe)
-        ("C-]" . godef-jump)
-        ("M-d" . godef-jump))
   :init
   (progn
     (evil-set-command-property 'godef-jump :jump t)
     (when (not (getenv "GOPATH")) (setenv "GOPATH" (expand-file-name "~/go/")))
-    (add-hook 'go-mode-hook #'flycheck-mode)
-    (setq godef-command (expand-file-name "~/go/bin/godef"))))
-
-(use-package company-go
-  :ensure t
-  :after go-mode
-  :init
-  (progn
-    (setq company-go-gocode-command (expand-file-name "~/go/bin/gocode"))
-    (add-hook 'go-mode-hook (lambda ()
-                              (set (make-local-variable 'company-backends) '(company-go))))))
+    (add-hook 'go-mode-hook #'lsp))) ; 'go get -u golang.org/x/tools/cmd/gopls' and add to path
 
 
 (provide 'init-go)
