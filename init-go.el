ca31b47521d40b77ceda860ec604a8e89ca1cc24
Patrick Haun <patrick@bomgar.email>
2019-05-10 12:36:26 +0200

    [fix] keybinding

--- init-go.el
+++ init-go.el
@@ -4,6 +4,7 @@
   :init
   (progn
     (evil-set-command-property 'godef-jump :jump t)
+    (evil-define-key 'normal go-mode-map (kbd "C-]") 'evil-goto-definition)
     (when (not (getenv "GOPATH")) (setenv "GOPATH" (expand-file-name "~/go/")))
     (add-hook 'go-mode-hook #'lsp))) ; 'go get -u golang.org/x/tools/cmd/gopls' and add to path
 
