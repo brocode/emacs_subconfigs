a0aad9e6cc88430ab41ae91a66ebbe6947bb33ab
Patrick Haun <patrick@bomgar.email>
2019-05-24 14:37:49 +0200

    [fix] _ is part of a word

--- init-rust.el
+++ init-rust.el
@@ -2,6 +2,8 @@
 (use-package rust-mode
   :ensure t
   :init
-  (add-hook 'rust-mode-hook #'lsp))
+  (add-hook 'rust-mode-hook #'lsp)
+  :config
+  (modify-syntax-entry ?_ "w" rust-mode-syntax-table))
 
 (provide 'init-rust)
