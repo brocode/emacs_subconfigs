02d67135445443c47b2e786815fa2a711c0b9e6c
Patrick Haun <patrick@bomgar.email>
2019-09-04 08:42:09 +0200

    [fix] goto definition

--- init-lsp.el
+++ init-lsp.el
@@ -9,7 +9,7 @@
   (progn
     (setq lsp-ui-sideline-enable nil)
     (setq lsp-ui-doc-position 'at-point)
-    (evil-define-key 'normal lsp-ui-mode-map (kbd "C-]") 'evil-goto-definition)
+    (evil-define-key 'normal lsp-ui-mode-map (kbd "C-]") 'lsp-find-definition)
     (evil-define-key 'normal lsp-ui-mode-map (kbd "C-c d") (lambda () (interactive) (progn
                                                                                  (if lsp-ui-doc-mode (lsp-ui-doc-mode -1)
                                                                                    (lsp-ui-doc-mode))
