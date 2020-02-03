7396aba01478a121646b70ccd3e179f39ab73c6a
Patrick Haun <patrick@bomgar.email>
2020-02-03 15:18:32 +0100

    [hack] get lombok to work for now

--- init-java.el
+++ init-java.el
@@ -4,6 +4,13 @@
   (progn
     (evil-define-key 'insert java-mode-map (kbd "<tab>") 'company-indent-for-tab-command)
     (evil-define-key 'insert java-mode-map (kbd "TAB") 'company-indent-for-tab-command)
+    (setq lsp-java-vmargs
+            `("-noverify"
+              "-Xmx1G"
+              "-XX:+UseG1GC"
+              "-XX:+UseStringDeduplication"
+              ,(concat "-javaagent:" "/opt/lombok/lombok.jar")
+              ,(concat "-Xbootclasspath/a:" "/opt/lombok/lombok.jar")))
     (add-hook 'java-mode-hook #'lsp)))
 
 
