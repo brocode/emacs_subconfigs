29ce32fd10b4900ca4fb8ddeee94555bbe6f7fc9
Maximilien Riehl <max@riehl.io>
2022-03-02 10:48:19 +0100

    [cleanup] remove jvm languages and python

--- init-kotlin.el
+++
@@ -1,8 +0,0 @@
-(use-package kotlin-mode
-  :ensure t
-  :init
-  (progn
-    (add-hook 'kotlin-mode-hook #'lsp)))
-
-
-(provide 'init-kotlin)
--- init-play-routes-mode.el
+++
@@ -1,5 +0,0 @@
-(use-package play-routes-mode
-  :ensure t
-  :defer t)
-
-(provide 'init-play-routes-mode)
